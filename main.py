988831e9327eeee5564bf14a3dbe8cadfcf1ccc5
abishekpandiyan <168189952+abishekpandiyan@users.noreply.github.com>
2024-05-17 10:24:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,87 +1,110 @@
 import random
-class GameBoard:
-  def __init__(self, board):
-    self.board = board
 
-  def get_letters_to_numbers():
-    letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, }
-    return letters_to_numbers
+# Singleton Design Pattern for GameSettings
+class GameSettings:
+    _instance = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(GameSettings, cls).__new__(cls)
+            cls._instance.ships_count = 2
+            cls._instance.board_size = 6
+        return cls._instance
 
-  def print_board(self):
-      print("  A B C D E F ")
-      print("  +-+-+-+-+-+")
-      row_number = 1
-      for row in self.board:
-          print("%d|%s|" % (row_number, "|".join(row)))
-          row_number += 1
-class Battleship:
-  def __init__(self, board):
-    self.board = board
+# Encapsulation, Inheritance, and Abstraction
+class Board:
+    def __init__(self, size):
+        self.size = size
+        self.board = [[" "] * size for _ in range(size)]
 
-  def create_ships(self):
-    for i in range(2):
-      self.x_row, self.y_column = random.randint(0,5), random.randint(0, 5)
-      while self.board[self.x_row][self.y_column] == "X":
-        self.x_row, self.y_column = random.randint(0, 5), random.randint(0, 5)
-      self.board[self.x_row][self.y_column] = "X"
-    return self.board
+    def print_board(self):
+        print("  " + " ".join([chr(i) for i in range(65, 65 + self.size)]))
+        print("  " + "+-" * self.size + "+")
+        for i, row in enumerate(self.board):
+            print(f"{i+1}|{'|'.join(row)}|")
 
-  def get_user_input(self):
-    try:
-      x_row = input("Enter the row of the ship: ")
-      while x_row not in '123456':
-          print('open your eyes and choose wisely')
-          x_row = input("now do it again you blind: ")
+    @staticmethod
+    def get_letters_to_numbers():
+        return {chr(i): i - 65 for i in range(65, 65 + 6)}
 
-      y_column = input("Enter the column letter of the ship: ").upper()
-      while y_column not in "ABCDEF":
-          print('open your eyes and choose wisely')
-          y_column = input("now stop playing around and choose the letter: ").upper()
-      return int(x_row) - 1, GameBoard.get_letters_to_numbers()[y_column]
-    except ValueError and KeyError:
-      print("stop playing around")
-      return self.get_user_input()
+# Inheritance
+class GameBoard(Board):
+    pass
 
-  def count_hit_ships(self):
-    hit_ships = 0
-    for row in self.board:
-      for column in row:
-        if column == "X":
-          hit_ships += 1
-    return hit_ships
+class GuessBoard(Board):
+    pass
 
-def RunGame(): 
-  computer_board = GameBoard([[" "] * 6 for i in range(6)])
-  user_guess_board = GameBoard([[" "] * 6 for i in range(6)])
-  Battleship.create_ships(computer_board)
- 
-  turns = 8
-  while turns > 0:
-    GameBoard.print_board(user_guess_board)
-   
-    user_x_row, user_y_column = Battleship.get_user_input(object)
-   
-    while user_guess_board.board[user_x_row][user_y_column] == "-" or user_guess_board.board[user_x_row][user_y_column] == "X":
-      print("been there ,done that")
-      user_x_row, user_y_column = Battleship.get_user_input(object)
-   
-    if computer_board.board[user_x_row][user_y_column] == "X":
-      print("You sunk 1 of my battleship!,now you have done it !")
-      user_guess_board.board[user_x_row][user_y_column] = "X"
-    else:
-      print("its my lucky day!")
-      user_guess_board.board[user_x_row][user_y_column] = "-"
-   
-    if Battleship.count_hit_ships(user_guess_board) == 5:
-      print("you know something its just my bad luck , congrats on your winning !")
-      break
-    else:
-      turns -= 1
-      print(f"You have {turns} turns remaining")
-      if turns == 0:
-        print("hahahahaha.........who is the boss?, its me.........sorry mate you lost")
-        GameBoard.print_board(user_guess_board)
-        break
+class ShipPlacementStrategy:
+    def place_ships(self, board, count):
+        raise NotImplementedError("This method should be overridden.")
+
+class RandomShipPlacement(ShipPlacementStrategy):
+    def place_ships(self, board, count):
+        for _ in range(count):
+            x_row, y_column = random.randint(0, board.size - 1), random.randint(0, board.size - 1)
+            while board.board[x_row][y_column] == "X":
+                x_row, y_column = random.randint(0, board.size - 1), random.randint(0, board.size - 1)
+            board.board[x_row][y_column] = "X"
+
+class BattleshipGame:
+    def __init__(self, placement_strategy):
+        self.settings = GameSettings()
+        self.computer_board = GameBoard(self.settings.board_size)
+        self.user_guess_board = GuessBoard(self.settings.board_size)
+        self.placement_strategy = placement_strategy
+        self.placement_strategy.place_ships(self.computer_board, self.settings.ships_count)
+
+    def get_user_input(self):
+        try:
+            x_row = input("Enter the row of the ship: ")
+            while x_row not in '123456':
+                print('Open your eyes and choose wisely')
+                x_row = input("Enter the row of the ship again: ")
+
+            y_column = input("Enter the column letter of the ship: ").upper()
+            while y_column not in "ABCDEF":
+                print('Open your eyes and choose wisely')
+                y_column = input("Enter the column letter again: ").upper()
+            return int(x_row) - 1, Board.get_letters_to_numbers()[y_column]
+        except (ValueError, KeyError):
+            print("Invalid input, try again.")
+            return self.get_user_input()
+
+    def count_hit_ships(self, board):
+        return sum(row.count("X") for row in board.board)
+
+    def run_game(self):
+        turns = 8
+        while turns > 0:
+            self.user_guess_board.print_board()
+            user_x_row, user_y_column = self.get_user_input()
+
+            while self.user_guess_board.board[user_x_row][user_y_column] in ["-", "X"]:
+                print("You've already guessed that location.")
+                user_x_row, user_y_column = self.get_user_input()
+
+            if self.computer_board.board[user_x_row][user_y_column] == "X":
+                print("You sunk 1 of my battleships!")
+                self.user_guess_board.board[user_x_row][user_y_column] = "X"
+            else:
+                print("It's my lucky day!")
+                self.user_guess_board.board[user_x_row][user_y_column] = "-"
+
+            if self.count_hit_ships(self.user_guess_board) == self.settings.ships_count:
+                print("Congratulations, you win!")
+                break
+            else:
+                turns -= 1
+                print(f"You have {turns} turns remaining")
+                if turns == 0:
+                    print("Game over! You lost.")
+                    self.user_guess_board.print_board()
+                    break
 
 if __name__ == '__main__':
-  RunGame()
+    # Factory Method Design Pattern for creating a BattleshipGame with RandomShipPlacement
+    def game_factory():
+        return BattleshipGame(RandomShipPlacement())
+
+    game = game_factory()
+    game.run_game()
